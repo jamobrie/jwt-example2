325ee3cd1c0360b9935e609a8e22aa6cbec11125
James O'Brien <jamesobrien442@gmail.com>
2022-01-12 20:08:31 +0000

    JWT Logic, almost working

---
+++ src/main/java/config/CSRFConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import service.SecretService;
+
+@Configuration
+public class CSRFConfig {
+
+    @Autowired
+    SecretService secretService;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CsrfTokenRepository jwtCsrfTokenRepository() {
+        return new JWTCsrfTokenRepository(secretService.getHS256SecretBytes());
+    }
+}
